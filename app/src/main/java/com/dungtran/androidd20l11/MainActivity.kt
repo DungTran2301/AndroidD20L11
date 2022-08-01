4f1bbd92783ed6022ccd0472c452c65ccc9ec7c1
Tuan Vo <dungtuan095@gmail.com>
2022-08-01 12:30:22 +0700

    Test commit

--- app/src/main/java/com/dungtran/androidd20l11/MainActivity.kt
+++ app/src/main/java/com/dungtran/androidd20l11/MainActivity.kt
@@ -2,10 +2,27 @@ package com.dungtran.androidd20l11
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+    private var fragmentManager: FragmentManager? = null
+    private var fragmentTransaction: FragmentTransaction? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addFragment()
+    }
+
+    private fun addFragment() {
+        fragmentManager = supportFragmentManager
+        fragmentTransaction = fragmentManager?.beginTransaction()
+        fragmentTransaction?.add(
+            R.id.frameContainer,
+            HomeFragment()
+        )
+        fragmentTransaction?.commit()
+
+
     }
 }
