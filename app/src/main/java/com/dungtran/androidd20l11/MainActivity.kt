7078ad78c949066fd7ee130214003bab2363aa04
Đỗ Xuân Hướng <dohuong0422@gmail.com>
2022-08-01 19:29:47 +0700

    test 4

--- app/src/main/java/com/dungtran/androidd20l11/MainActivity.kt
+++ app/src/main/java/com/dungtran/androidd20l11/MainActivity.kt
@@ -2,9 +2,16 @@ package com.dungtran.androidd20l11
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/dungtran/androidd20l11/TabPlayFragment.kt
+++ app/src/main/java/com/dungtran/androidd20l11/TabPlayFragment.kt
@@ -1,9 +1,32 @@
 package com.dungtran.androidd20l11
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
 class TabPlayFragment : Fragment(R.layout.fragment_tab_play) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val tabLayout = getView()?.findViewById<TabLayout>(R.id.tabLayout)
+        val viewPager2 = getView()?.findViewById<ViewPager2>(R.id.viewPager2)
+        val adapter = ViewPaperAdapter(childFragmentManager, lifecycle)
+        viewPager2?.adapter = adapter
+        tabLayout?.let {
+            viewPager2?.let { it1 ->
+                TabLayoutMediator(it, it1) { tab, position ->
+                    when (position) {
+                        0 -> {
+                            tab.text = "Favourite"
+                        }
+                        1 -> {
+                            tab.text = "Your Music"
+                        }
+                    }
+                }
+            }
+        }?.attach()
+    }
 }
