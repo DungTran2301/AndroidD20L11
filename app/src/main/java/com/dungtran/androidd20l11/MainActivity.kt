8e9ffdcc14ccb0f90f5cd1ba283b4dfc663322ec
Đỗ Xuân Hướng <dohuong0422@gmail.com>
2022-08-01 13:45:00 +0700

    test 2

--- app/src/main/java/com/dungtran/androidd20l11/MainActivity.kt
+++ app/src/main/java/com/dungtran/androidd20l11/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.dungtran.androidd20l11
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tabHomeFragment = TabHomeFragment()
+        val tabPlayFragment = TabPlayFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flTabContent, tabHomeFragment)
+            commit()
+        }
+
+        val tiHome = findViewById<TabItem>(R.id.tiHome)
+        val tiPlay = findViewById<TabItem>(R.id.tiPlay)
+
+        tiHome.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flTabContent, tabHomeFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        tiPlay.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flTabContent, tabPlayFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
