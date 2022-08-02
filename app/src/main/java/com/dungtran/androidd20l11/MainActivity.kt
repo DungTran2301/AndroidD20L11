b8b148fb58433691941df74bd5f4de2281eb2e59
VuNAP <vuvovan2k2>
2022-08-02 09:11:08 +0700

    [EDIT] MainActivity

--- app/src/main/java/com/dungtran/androidd20l11/MainActivity.kt
+++ app/src/main/java/com/dungtran/androidd20l11/MainActivity.kt
@@ -2,10 +2,26 @@ package com.dungtran.androidd20l11
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dungtran.androidd20l11.adapter.PlayAdapter
+import com.dungtran.androidd20l11.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.apply {
+            vpTabBarFavorite.adapter = PlayAdapter(supportFragmentManager, lifecycle)
+            TabLayoutMediator(tabLayout, vpTabBarFavorite) { tab, position ->
+                when (position) {
+                    0 -> tab.text = "Favorite"
+                    1 -> tab.text = "Your Music"
+                }
+            }.attach()
+        }
+
+
+        setContentView(binding.root)
     }
 }
