8e6d21668a51e8b02a517bb37f17dd8d28e32278
Tuan Vo <dungtuan095@gmail.com>
2022-08-02 16:24:54 +0700

    Test commit

--- app/src/main/java/com/dungtran/androidd20l11/EditProfileFragment.kt
+++ app/src/main/java/com/dungtran/androidd20l11/EditProfileFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.fragment.findNavController
 import com.dungtran.androidd20l11.databinding.FragmentEditProfileBinding
 
 
@@ -18,7 +20,31 @@ class EditProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentEditProfileBinding.inflate(inflater,container,false)
-
+        setData()
+        binding.btnSaveProfile.setOnClickListener {
+            clickOn()
+        }
         return binding.root
+    }
+
+    private fun setData() {
+        val user = arguments?.getSerializable("data_user") as User
+        binding.userName.setText(user.userName)
+        binding.phoneNum.setText(user.phoneNum)
+        binding.email.setText(user.email)
+    }
+
+    private fun clickOn() {
+        val bundle = Bundle()
+        bundle.putSerializable(
+            "user",
+            User(
+                binding.userName.text.toString(),
+                binding.phoneNum.text.toString(),
+                binding.email.text.toString()
+            )
+        )
+        setFragmentResult("dataUser",bundle)
+        findNavController().navigate(R.id.action_editProfileFragment_to_settingFragment2)
     }
 }
--- app/src/main/java/com/dungtran/androidd20l11/SettingFragment.kt
+++ app/src/main/java/com/dungtran/androidd20l11/SettingFragment.kt
@@ -1,24 +1,50 @@
 package com.dungtran.androidd20l11
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
+import androidx.navigation.fragment.findNavController
 import com.dungtran.androidd20l11.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
 
     private lateinit var binding: FragmentSettingBinding
 
-
+    private var user = User("Jessica","0981234567","jessica098@gmail.com")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
        binding = FragmentSettingBinding.inflate(inflater,container,false)
-        setData(User("Jessica","0981234567","jessica098@gmail.com"))
+        getData()
+        setData(user)
+        binding.btnProfile.setOnClickListener {
+            clickOn()
+        }
         return binding.root
+    }
+
+    private fun getData() {
+        setFragmentResultListener("dataUser"){_,bundle->
+            user = bundle.getSerializable("user") as User
+
+        }
+    }
+
+    private fun clickOn() {
+        val bundle = Bundle()
+        bundle.putSerializable(
+            "data_user",
+            User(
+                binding.userName.text.toString(),
+                binding.phoneNum.text.toString(),
+                binding.email.text.toString()
+            )
+        )
+        findNavController().navigate(R.id.action_settingFragment2_to_editProfileFragment,bundle)
     }
 
     private fun setData(user: User) {
