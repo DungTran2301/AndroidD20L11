f6448980909aac86fe9b24691c7aab6607164b82
Tuan Vo <dungtuan095@gmail.com>
2022-08-02 11:48:18 +0700

    Test commit

--- app/src/main/java/com/dungtran/androidd20l11/MainActivity.kt
+++ app/src/main/java/com/dungtran/androidd20l11/MainActivity.kt
@@ -2,7 +2,6 @@ package com.dungtran.androidd20l11
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
